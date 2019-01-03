064ab15a5ca3013400cc9b31691f1703850a6c91
ELehotzky <Lehotzky.Erica@gmail.com>
2019-01-02 23:07:20 -0600

    * ProgramFlow.assets.cache: issues with run
    * Program.cs:
    * project.assets.json:
    * ProgramFlow.csproj.nuget.cache:
    * ProgramFlow.csproj.nuget.g.props:

--- ProgramFlow/Program.cs
+++ ProgramFlow/Program.cs
@@ -6,7 +6,23 @@ namespace ProgramFlow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int ourNum = 10;
+
+            Console.WriteLine("Please guess the number");
+            String answer = Console.ReadLine();
+            int guessNum = 0;
+            Int32.TryParse(answer, out guessNum);
+
+            if (guessNum == ourNum)
+            {
+                Console.WriteLine("Correct! Good job!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you didn't guess the right number!");
+            }
+            Console.WriteLine("Press any key to quit");
+            Console.ReadLine();
         }
     }
 }
